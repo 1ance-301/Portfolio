b560a18b31bd65a0932d2d43ccd04e785b6400fd
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2022-04-21 23:13:56 -0400

    basic layout and About page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Lance Bontrager</h1>
+        <nav>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Resume</li>
+          <li>Contact</li>
+        </nav>
       </header>
+
+      <About />
+      <Portfolio />
+      <Resume />
+      <Contact />
+
+      <footer>Made with ❤︎ by Lance Bontrager</footer>
     </div>
   );
 }
---
+++ my-portfolio/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className="about">
+            <h2>About</h2>
+
+            <p>I am a full stack freelance web developer. I have completed and graduated Butler Bootcamp. I am currently living in Middlebury, Indiana, but I have thought about relocating to a different location. My desire is to learn as much code as I can and be able to write it competitively.</p>
+        </div>
+    )
+};
+
+export default About;
